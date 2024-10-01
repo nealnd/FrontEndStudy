d08712cfc02d6f60dc1e613fbcec7296cd41d333
Neal <neal.nd@outlook.com>
2024-10-01 13:45:39 -0700

    the foundation study of javascript,css and html

---
+++ Foundation/Practice_33/script.js
@@ -0,0 +1,70 @@
+
+const container = document.querySelector('.container');
+const addnote = document.querySelector('.add');
+
+
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if (notes) {
+    notes.forEach(note => addNewNote(note))
+}
+
+function addNewNote(text = '') {
+    const noteE = document.createElement('div');
+    noteE.className += `note`;
+    noteE.innerHTML = `
+
+    <div class="head">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+    <textarea name="content" class="content">${text}</textarea>
+
+
+`;
+
+    container.appendChild(noteE);
+
+    const editBtn = noteE.querySelector('.edit');
+    const deleteBtn = noteE.querySelector('.delete');
+    const contentArea = noteE.querySelector('.content');
+
+    contentArea.addEventListener('input', () => {
+        updateLS();
+    })
+
+    editBtn.addEventListener('click', () => {
+        updateLS();
+        contentArea.classList.toggle('readonly');
+    })
+
+    deleteBtn.addEventListener('click', () => {
+        noteE.remove();
+        updateLS();
+    })
+}
+
+addnote.addEventListener('click', () => {
+    addNewNote();
+
+
+})
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea')
+
+    const notes = []
+
+    notesText.forEach(note => notes.push(note.value))
+
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+
+// <div class="note">
+//     <div class="head">
+//         <button class="edit"><i class="fas fa-edit"></i></button>
+//         <button class="delete"><i class="fas fa-trash-alt"></i></button>
+//     </div>
+//     <textarea name="content" id="content"></textarea>
+// </div>
