a4100d1c30e9f755e43c8755c98d176ee9d84f92
Lexie <m18140663022_1@163.com>
2024-10-19 16:23:10 -0700

    mall

---
+++ Vue/mall/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+
+<HeaderTop></HeaderTop>
+
+</template>
+
+<script setup>
+import HeaderTop from '../src/components/HeaderTop.vue'
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/mall/src/components/HeaderTop.vue
@@ -0,0 +1,21 @@
+<template>
+    <header id="head_top">
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+#head_top {
+    background-color: #3190e8;
+    position: fixed;
+    z-index: 100;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 1.95rem;
+
+}
+</style>
