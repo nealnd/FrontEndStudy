07ac305fa93aa70b4d376f46d82fbb6a91b075f1
Neal <neal.nd@outlook.com>
2024-10-22 20:07:00 -0700

    finish the category page

---
+++ Vue/mall/src/views/Category/composables/useCategory.js
@@ -0,0 +1,29 @@
+import { getCategoryAPI } from "@/apis/category";
+import { ref } from "vue";
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+import { onBeforeRouteUpdate } from "vue-router";
+
+export function useCategory() {
+  const categoryData = ref({});
+  const route = useRoute();
+  const getCategory = async (id = route.params.id) => {
+    const res = await getCategoryAPI(id);
+    categoryData.value = res.result;
+  };
+
+  onMounted(() => {
+    getCategory();
+
+  });
+
+  onBeforeRouteUpdate((to) => {
+    getCategory(to.params.id);
+   
+  });
+
+  return { 
+    categoryData
+
+   };
+}
--- Vue/mall/src/views/Layout/components/LayoutFixed.vue
+++ Vue/mall/src/views/Layout/components/LayoutFixed.vue
@@ -14,10 +14,10 @@ const categoryStore=useCategoryStore();
       <!-- 导航区域 -->
       <ul class="app-header-nav ">
         <li class="home">
-          <RouterLink to="/">首页</RouterLink>
+          <RouterLink  to="/">首页</RouterLink>
         </li>
         <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
-        <RouterLink to="/">{{ item.name }}</RouterLink>
+        <RouterLink active-class="active" :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         </li>
     
       </ul>
