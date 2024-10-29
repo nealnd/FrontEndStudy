b4e5a5bc8ce1adc1bbe8b36f71d546e6d56eef17
Lexie <m18140663022_1@163.com>
2024-10-28 18:17:19 -0700

    not finish

--- Vue/mall/src/router/index.js
+++ Vue/mall/src/router/index.js
@@ -5,6 +5,8 @@ import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
+import CarList from '@/views/CartList/index.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -28,6 +30,10 @@ const router = createRouter({
         {
           path:'details/:id',
           component:Detail,
+        },
+        {
+          path:'cartlist',
+          component:CarList
         }
 
       ]
